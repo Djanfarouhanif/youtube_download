93ace3995cd6fa90a713e79f0d01565c5a859540
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-12-01 23:20:08 +0000

    changement de disgn

--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -12,8 +12,32 @@ module.exports = {
         'custom' : '0px 20px 20px 0',
         'cust': ' 24px 0 0 24px'
         
+      },
+      animation: {
+        wiggle : 'wiggle 5s ease infinite'
+      },
+      keyframes: {
+        wiggle: { 
+          '0%': { backgroundPosition : '0% 50% '},
+          '50%': { backgroundPosition : '100% 50%'},
+          '100%': { backgroundPosition : '0% 50%'},
+        }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, addUtilities, addComponents}){
+      // Ajout des styles de base 
+      addBase({
+        'p, h1': {
+          color: 'rgba(17, 216, 123, 0.923)',
+        },
+      });
+      addUtilities({
+        '.glow-effect': {
+          boxShadow: '0 0 15px rgba(58, 24, 139, 0.8), 0 0 50px rgba(58, 24, 139, 0.5)',
+        },
+      })
+    }
+  ],
 }
