ac30a6523b8557c968a34f52bbf8965fc228fc32
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-12-02 23:45:42 +0000

    j'ai un implementer les fonction pour envoyer les requetes

--- core/src/app/api.service.ts
+++ core/src/app/api.service.ts
@@ -8,6 +8,24 @@ import { catchError } from 'rxjs/operators'
 })
 export class ApiService {
 
- 
+ private apiUrlVideo = 'http://127.0.0.1:8000/download-video/';
+ private apiUrlData  = 'http://127.0.0.1:8000/video-data/'; 
+      constructor( private http:HttpClient){}
+
+      // Fonction pour télécharger la video
+      downloadVideo(data:any):Observable<any>{
+
+        return this.http.post(this.apiUrlVideo, data , {responseType: "blob"})
+      }
+
+      // Fonction pour recuper le titre et la minature
+      getData(data:any):Observable<any>{
+        const headers = new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+        return this.http.post(this.apiUrlData, data, {headers})
+      }
+
+
 
 }
--- core/src/app/home/home.component.ts
+++ core/src/app/home/home.component.ts
@@ -15,13 +15,41 @@ export class HomeComponent {
       success = false;
       searchControl: FormControl;
       load:boolean = false;
+      thumbnailUrl: string = '';
 
 
-      constructor(){
+      constructor( private apiService:ApiService){
         this.searchControl = new FormControl('');
         this.searchControl.valueChanges.pipe(debounceTime(500)).subscribe(value=>{
-              this.load = !this.load
           
+              this.load = !this.load // Activer la bar de téléchagement 
+              const data = {"video_url": value}
+              this.apiService.getData(data).subscribe({
+                next: response => {
+                  this.load = false;
+                  console.log(`title: ${response.success.title}`);
+                  this.thumbnailUrl = response.success.Thumbnail
+                },
+                error: err => {
+                  this.load = false;
+                  console.log(`error: ${err}`);
+                }
+              });
+          
+        });
+      };
+
+      download(){
+        const data = {"video_url": this.searchControl.value};
+        console.log(this.searchControl.value);
+
+        this.apiService.downloadVideo(data).subscribe({
+          next: response => { 
+            console.log("success");
+          },
+          error: err => {
+            console.log(`error ${err}`);
+          }
         })
       }
 
